841bc1d373af7fb853493d32c0fcc95d112ffde9
Emyjakarta <ukonud4@gmail.com>
2024-07-16 22:13:09 +0100

    App is functional

--- dynamic/app.py
+++ dynamic/app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, request, render_template, redirect, url_for
 from models.user import User
 from sqlalchemy.orm import sessionmaker
@@ -6,7 +7,19 @@ from sqlalchemy import create_engine
 app = Flask(__name__)
 
 # Database setup
-engine = create_engine('mysql+mysqlconnector://football_scout_dev:football_scout_dev_pwd@localhost/football_scout_dev_db')
+# engine = create_engine('mysql+mysqlconnector://football_scout_dev:football_scout_dev_pwd@localhost/football_scout_dev_db')
+
+# Database setup
+# Extract the PostgreSQL connection details from environment variables
+user = os.getenv('FOOTBALL_SCOUT_DEV_PGSQL_USER', 'football_scout_dev')
+password = os.getenv('FOOTBALL_SCOUT_DEV_PGSQL_PWD', '8i0QuEi2hDvNDyUgmQpBY0tA2ztryywF')
+host = os.getenv('FOOTBALL_SCOUT_DEV_PGSQL_HOST', 'dpg-cqarnd08fa8c73asb9h0-a.oregon-postgres.render.com')
+database = os.getenv('FOOTBALL_SCOUT_DEV_PGSQL_DB', 'football_scout_dev_db')
+
+# Create the engine using the PostgreSQL connection string
+DATABASE_URL = f'postgresql://{user}:{password}@{host}/{database}'
+engine = create_engine(DATABASE_URL)
+
 Session = sessionmaker(bind=engine)
 session = Session()
 
