908b624e7a0aebafcce0cdbea5008e60c20bb1bc
JohnIanOngayi <ongayijohnian@gmail.com>
2024-07-21 15:44:21 +0300

    Deleted sqlite db

---
+++ dynamic_site/__init__.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+"""
+Initializes the dynamic_site package
+"""
+
+from models import storage
+from models.scout import Scout
+from models.player import Player
+from models.club import Club
+from models.country import Country
+from models.post import Post
+from models.like import Like
+from models.comment import Comment
+from models.position import Position
+from models.user import User
