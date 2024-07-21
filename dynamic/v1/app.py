5b7e420abd14387535b9b02578cf913f5b70e3a9
Emyjakarta <ukonud4@gmail.com>
2024-07-21 15:32:09 +0100

    Corrected the error in the engine for app.py

--- dynamic/v1/app.py
+++ dynamic/v1/app.py
@@ -42,7 +42,7 @@ if not app.blueprints.get('app_views'):
 # engine = create_engine('mysql+mysqlconnector://football_scout_dev:football_scout_dev_pwd@localhost/football_scout_dev_db')
 
 # Database setup
-Extract the PostgreSQL connection details from environment variables
+# Extract the PostgreSQL connection details from environment variables
 user = os.getenv('FOOTBALL_SCOUT_DEV_PGSQL_USER', 'football_scout_dev')
 password = os.getenv('FOOTBALL_SCOUT_DEV_PGSQL_PWD', '8i0QuEi2hDvNDyUgmQpBY0tA2ztryywF')
 host = os.getenv('FOOTBALL_SCOUT_DEV_PGSQL_HOST', 'dpg-cqarnd08fa8c73asb9h0-a.oregon-postgres.render.com')
